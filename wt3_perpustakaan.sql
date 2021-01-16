9e58b3bbd850ef1cd2f2a4896b0578e3b7838205
Gendhika Mahesajayya <gendhika508@gmail.com>
2021-01-16 10:53:10 +0700

    'server'

--- wt3_perpustakaan.sql
+++ wt3_perpustakaan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 15 Jan 2021 pada 14.32
+-- Waktu pembuatan: 16 Jan 2021 pada 03.31
 -- Versi server: 10.1.36-MariaDB
 -- Versi PHP: 5.6.38
 
@@ -50,6 +50,64 @@ INSERT INTO `buku` (`kd_buku`, `judul`, `isbn`, `cover`, `penulis`, `tahun`, `ka
 ('003', 'Five Nights at Freddy\'s: The Silver Eyes', '978-1338134377', 'https://images-na.ssl-images-amazon.com/images/I/51kkeE8Jv+L._SX326_BO1,204,203,200_.jpg', 'Scott Cawthon and Kira Breed-Wrisley', '2015 ', 'Novel', 'The book follows a young woman named Charlotte, who reunites with her childhood friends on the anniv', NULL),
 ('004', 'Gravity Falls: Journal 3', '978-1484746691', 'https://static.wikia.nocookie.net/gravityfalls/images/9/90/Journal_3%27s_dust_cover_1.jpg/revision/latest/scale-to-width-down/700?cb=20160718094558', 'Rob Renzetti and Alex Hirsch', '2016', 'Novel', 'Journal 3 brims with every page ever seen on the show plus all-new pages with monsters and secrets, ', NULL),
 ('005', 'Fantastic Beasts and Where to Find Them', '0-439-29501-7', 'https://upload.wikimedia.org/wikipedia/en/8/8d/Fantastic_beasts.JPG', 'J. K. Rowling', '2010', 'Novel', 'Fantastic Beasts purports to be a reproduction of a textbook owned by Harry Potter and written by ma', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `get_detail_account`
+--
+
+CREATE TABLE `get_detail_account` (
+  `id_account` varchar(255) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `level_access_user` tinyint(1) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `allow_edit` tinyint(1) NOT NULL,
+  `allow_delete` tinyint(1) NOT NULL,
+  `allow_update` tinyint(1) NOT NULL,
+  `allow_create` tinyint(1) NOT NULL,
+  `is_deleted` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `get_detail_account`
+--
+
+INSERT INTO `get_detail_account` (`id_account`, `nama`, `password`, `email`, `level_access_user`, `username`, `allow_edit`, `allow_delete`, `allow_update`, `allow_create`, `is_deleted`) VALUES
+('001', 'andre', '@dmin', 'andre3@gmail.com', 1, 'andreas', 1, 1, 1, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kategori_buku`
+--
+
+CREATE TABLE `kategori_buku` (
+  `kd_kategoribuku` varchar(255) NOT NULL,
+  `nm_kategoribuku` varchar(255) NOT NULL,
+  `is_deleted` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `kategori_buku`
+--
+
+INSERT INTO `kategori_buku` (`kd_kategoribuku`, `nm_kategoribuku`, `is_deleted`) VALUES
+('01', 'novel', 1),
+('02', 'komik', 1),
+('03', 'ensiklopedia', 1),
+('04', 'dongeng', 1),
+('05', 'biography', 1),
+('06', 'diary', 1),
+('07', 'fotography', 1),
+('08', 'kamus', 1),
+('09', 'karya ilmiah', 1),
+('10', 'panduan', 1),
+('11', 'atlas', 1),
+('12', 'teks', 1),
+('13', 'mewarnai', 1);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
